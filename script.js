f9a26cf24bd25bf439498b817cb83439b8ef96d7
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2024-11-14 10:51:22 -0700

    Added functionality to tally the flavors inputed

---
+++ script.js
@@ -0,0 +1,18 @@
+let flavors = prompt(
+  "What are your favorite Froyo flavors? Please seperate falvors by using a comma",
+  "Berry"
+);
+
+let flavorsClean = flavors.replace(/ /g, "");
+let flavorsSplit = flavorsClean.split(",");
+const FlavorTotal = {};
+
+for (let i = 0; i < flavorsSplit.length; i++) {
+  if (flavorsSplit[i] in FlavorTotal) {
+    FlavorTotal[flavorsSplit[i]] += 1;
+  } else {
+    FlavorTotal[flavorsSplit[i]] = 1;
+  }
+}
+console.log(typeof FlavorTotal);
+console.log(FlavorTotal);
